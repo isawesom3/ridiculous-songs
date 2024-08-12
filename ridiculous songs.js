f93b599f292444c6b4d88ace1e9dd30cad43626c
Squiddyinkster <almighty.alexanderz@gmail.com>
2024-08-12 11:41:01 -0400

    Create ridiculous songs.js

---
+++ ridiculous songs.js
@@ -0,0 +1,19 @@
+let adjective1 = "bouncy"; //little
+let noun1 = "cow";          //star
+let noun2 = "pizza";        //world
+let adjective2 = "smelly";  //high
+let shape1 = "triangle";    //diamond
+let place1 = "kitchen";     //place
+
+let song = "Twinkle, twinkle, " + adjective1;
+song += " " + noun1 + ", ";
+song += "how I wonder what you are. ";
+song += "Up above the " + noun2 + " so ";
+song += adjective2;
+song += ", like a " + shape1 + " in the " + place1;
+song += ". Twinkle, twinkle, " + adjective1 + " ";
+song += noun1 + ", how I wonder what you are.";
+
+
+// alert(song);
+console.log(song); // Output the song to the consolev
